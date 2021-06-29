e1cff8e75f0c44ead09cf3da58dc49a3b4ffb399
lyj00912 <3122742760@qq.com>
2021-06-29 11:09:53 +0800

    renamed and refactored WIndowsSimuLayer things

---
+++ WindowsSimuLayer/WindowsHelper.cpp
@@ -0,0 +1,61 @@
+#include "WindowsHelper.h"
+
+void WSL_WindowsHelper::GetScreenResolution(i32& resultWidth, i32& resultHeight)
+{
+	// Get Screen HDC
+	HDC hdcScreen;
+	hdcScreen = CreateDC(L"DISPLAY", NULL, NULL, NULL);
+	// Get X and Y
+	resultWidth = GetDeviceCaps(hdcScreen, HORZRES);
+	resultHeight = GetDeviceCaps(hdcScreen, VERTRES);
+	// Release HDC
+	if (csNull != hdcScreen) {
+		DeleteDC(hdcScreen);
+	}
+}
+
+WSL_WindowsHelper::WSL_WindowsHelper()
+{
+	GetScreenResolution(screenWidth, screenHeight);
+	unit = screenHeight / 30;
+
+	windowHeight = 26 * unit;
+	windowWidth = CS_iclamp(0, windowHeight / 9 * 16, screenWidth - 10);
+
+	leftMargin = (screenWidth - windowWidth) / 2;
+	topMargin = (screenHeight - windowHeight) / 2;
+}
+
+void WSL_WindowsHelper::RegisterAndCreateWindow
+(
+	WNDPROC MsgProc, HINSTANCE& hInstance,
+	const wchar_t* WindowClassName, const wchar_t* WindowTitle
+)
+{
+	// Register Window
+	wc = {
+		sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0, 0,
+		GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
+		WindowClassName, NULL
+	};
+	wc.hIconSm = LoadIcon(wc.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CROSSBUFFERFORWINDOWS));
+	RegisterClassEx(&wc);
+
+	// Create Window
+	wr.left = leftMargin;
+	wr.right = windowWidth + wr.left;
+	wr.top = topMargin;
+	wr.bottom = windowHeight + wr.top;
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+	hWnd = CreateWindowW(
+		WindowClassName, WindowTitle,
+		WS_OVERLAPPEDWINDOW,
+		wr.left, wr.top, wr.right - wr.left, wr.bottom - wr.top,
+		NULL, NULL, wc.hInstance, NULL
+	);
+
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+}
---
+++ WindowsSimuLayer/WindowsHelper.h
@@ -0,0 +1,36 @@
+#ifndef __CSBF_WindowsSimuLayer_Win32Parts__
+#define __CSBF_WindowsSimuLayer_Win32Parts__
+
+#include <Windows.h>
+#include "Resource.h"
+#include "../CrossBufferLayer/BasicDataTypeDeclarations.h"
+
+
+typedef LRESULT(*MsgProcType) (HWND, UINT, WPARAM, LPARAM);
+
+class WSL_WindowsHelper {
+public:
+	i32 screenWidth;
+	i32 screenHeight;
+	i32 unit;
+	i32 windowWidth;
+	i32 windowHeight;
+	i32 leftMargin;
+	i32 topMargin;
+
+	WNDCLASSEX wc;
+	RECT wr;
+	HWND hWnd;
+
+	void GetScreenResolution(i32& resultWidth, i32& resultHeight);
+
+	WSL_WindowsHelper();
+	void RegisterAndCreateWindow
+	(
+		WNDPROC MsgProc, HINSTANCE& hInstance,
+		const wchar_t* WindowClassName, const wchar_t* WindowTitle
+	);
+};
+
+
+#endif
