9e3689c113dea83206c4bffd623dc1f9b40d5f73
lyj00912 <3122742760@qq.com>
2021-07-01 17:09:47 +0800

    fixed some bugs in WindowsSimuLayer

--- WindowsSimuLayer/CrossBufferForWindows.cpp
+++ WindowsSimuLayer/CrossBufferForWindows.cpp
@@ -42,7 +42,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
 
 	// Initialize CrossBufferLayer and WIndowsSimuLayer Objects
-	windowsHelper = WSL_WindowsHelper(MsgProc, hInstance, WindowTitle, WindowTitle);
+	windowsHelper = WSL_WindowsHelper(MsgProc, hInstance, (wchar_t*)WindowTitle, (wchar_t*)WindowTitle);
 	d3dHelper     = WSL_D3DHelper(windowsHelper.hWnd);
 	fb            = CS_FrameBuffer(windowsHelper.windowWidth, windowsHelper.windowHeight);
 	kb            = CS_Keyboard();
@@ -106,7 +106,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	/* After the Main Loop */
 	{
 		// Release All the Variables
-		windowsHelper.Unregister(WindowTitle);
+		windowsHelper.Unregister((wchar_t*)WindowTitle);
 		d3dHelper.Release();
 		return 0;
 	}
