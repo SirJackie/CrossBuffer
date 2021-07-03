fbcc8bcb3afe21eb3d705ca4b3518fcfa4bc03ed
lyj00912 <3122742760@qq.com>
2021-07-03 20:01:08 +0800

    commented useless codes in WindowsSimuLayer/KeyboardHelper.cpp

--- WindowsSimuLayer/KeyboardHelper.cpp
+++ WindowsSimuLayer/KeyboardHelper.cpp
@@ -17,11 +17,11 @@ void WSL_KeyboardHelper::MoveWinBufIntoKeyBuf()
 	kb.SimuLayerSetKeyState(CSK_F7, windowsKeyBuffer[118]);
 	kb.SimuLayerSetKeyState(CSK_F8, windowsKeyBuffer[119]);
 	kb.SimuLayerSetKeyState(CSK_F9, windowsKeyBuffer[120]);
-	kb.SimuLayerSetKeyState(CSK_F10, windowsKeyBuffer[26]);
-	kb.SimuLayerSetKeyState(CSK_F11, windowsKeyBuffer[28]);
-	kb.SimuLayerSetKeyState(CSK_F12, windowsKeyBuffer[29]);
+	// kb.SimuLayerSetKeyState(CSK_F10, windowsKeyBuffer[26]);
+	// kb.SimuLayerSetKeyState(CSK_F11, windowsKeyBuffer[28]);
+	// kb.SimuLayerSetKeyState(CSK_F12, windowsKeyBuffer[29]);
 
-	kb.SimuLayerSetKeyState(CSK_BackQuote, windowsKeyBuffer[25]);
+	// kb.SimuLayerSetKeyState(CSK_BackQuote, windowsKeyBuffer[25]);
 	kb.SimuLayerSetKeyState(CSK_Num1, windowsKeyBuffer[49]);
 	kb.SimuLayerSetKeyState(CSK_Num2, windowsKeyBuffer[50]);
 	kb.SimuLayerSetKeyState(CSK_Num3, windowsKeyBuffer[51]);
@@ -32,8 +32,8 @@ void WSL_KeyboardHelper::MoveWinBufIntoKeyBuf()
 	kb.SimuLayerSetKeyState(CSK_Num8, windowsKeyBuffer[56]);
 	kb.SimuLayerSetKeyState(CSK_Num9, windowsKeyBuffer[57]);
 	kb.SimuLayerSetKeyState(CSK_Num0, windowsKeyBuffer[48]);
-	kb.SimuLayerSetKeyState(CSK_Minus, windowsKeyBuffer[10]);
-	kb.SimuLayerSetKeyState(CSK_Plus, windowsKeyBuffer[11]);
+	// kb.SimuLayerSetKeyState(CSK_Minus, windowsKeyBuffer[10]);
+	// kb.SimuLayerSetKeyState(CSK_Plus, windowsKeyBuffer[11]);
 	kb.SimuLayerSetKeyState(CSK_Backspace, windowsKeyBuffer[8]);
 
 	kb.SimuLayerSetKeyState(CSK_Tab, windowsKeyBuffer[9]);
@@ -47,9 +47,9 @@ void WSL_KeyboardHelper::MoveWinBufIntoKeyBuf()
 	kb.SimuLayerSetKeyState(CSK_I, windowsKeyBuffer[73]);
 	kb.SimuLayerSetKeyState(CSK_O, windowsKeyBuffer[79]);
 	kb.SimuLayerSetKeyState(CSK_P, windowsKeyBuffer[80]);
-	kb.SimuLayerSetKeyState(CSK_LeftSquareBracket, windowsKeyBuffer[12]);
-	kb.SimuLayerSetKeyState(CSK_RightSquareBracket, windowsKeyBuffer[14]);
-	kb.SimuLayerSetKeyState(CSK_BackSlash, windowsKeyBuffer[15]);
+	// kb.SimuLayerSetKeyState(CSK_LeftSquareBracket, windowsKeyBuffer[12]);
+	// kb.SimuLayerSetKeyState(CSK_RightSquareBracket, windowsKeyBuffer[14]);
+	// kb.SimuLayerSetKeyState(CSK_BackSlash, windowsKeyBuffer[15]);
 
 	kb.SimuLayerSetKeyState(CSK_CapsLock, windowsKeyBuffer[20]);
 	kb.SimuLayerSetKeyState(CSK_A, windowsKeyBuffer[65]);
@@ -61,8 +61,8 @@ void WSL_KeyboardHelper::MoveWinBufIntoKeyBuf()
 	kb.SimuLayerSetKeyState(CSK_J, windowsKeyBuffer[74]);
 	kb.SimuLayerSetKeyState(CSK_K, windowsKeyBuffer[75]);
 	kb.SimuLayerSetKeyState(CSK_L, windowsKeyBuffer[76]);
-	kb.SimuLayerSetKeyState(CSK_Semicolon, windowsKeyBuffer[18]);
-	kb.SimuLayerSetKeyState(CSK_Quote, windowsKeyBuffer[21]);
+	// kb.SimuLayerSetKeyState(CSK_Semicolon, windowsKeyBuffer[18]);
+	// kb.SimuLayerSetKeyState(CSK_Quote, windowsKeyBuffer[21]);
 	kb.SimuLayerSetKeyState(CSK_Enter, windowsKeyBuffer[13]);
 
 	kb.SimuLayerSetKeyState(CSK_Shift, windowsKeyBuffer[16]);
@@ -73,17 +73,17 @@ void WSL_KeyboardHelper::MoveWinBufIntoKeyBuf()
 	kb.SimuLayerSetKeyState(CSK_B, windowsKeyBuffer[66]);
 	kb.SimuLayerSetKeyState(CSK_N, windowsKeyBuffer[78]);
 	kb.SimuLayerSetKeyState(CSK_M, windowsKeyBuffer[77]);
-	kb.SimuLayerSetKeyState(CSK_Comma, windowsKeyBuffer[22]);
-	kb.SimuLayerSetKeyState(CSK_FullStop, windowsKeyBuffer[23]);
-	kb.SimuLayerSetKeyState(CSK_Slash, windowsKeyBuffer[24]);
-	kb.SimuLayerSetKeyState(CSK_RightShift, windowsKeyBuffer[123]);
+	// kb.SimuLayerSetKeyState(CSK_Comma, windowsKeyBuffer[22]);
+	// kb.SimuLayerSetKeyState(CSK_FullStop, windowsKeyBuffer[23]);
+	// kb.SimuLayerSetKeyState(CSK_Slash, windowsKeyBuffer[24]);
+	// kb.SimuLayerSetKeyState(CSK_RightShift, windowsKeyBuffer[123]);
 
 	kb.SimuLayerSetKeyState(CSK_Ctrl, windowsKeyBuffer[17]);
 	kb.SimuLayerSetKeyState(CSK_Win, windowsKeyBuffer[91]);
-	kb.SimuLayerSetKeyState(CSK_Alt, windowsKeyBuffer[125]);
+	// kb.SimuLayerSetKeyState(CSK_Alt, windowsKeyBuffer[125]);
 	kb.SimuLayerSetKeyState(CSK_Space, windowsKeyBuffer[32]);
-	kb.SimuLayerSetKeyState(CSK_RightAlt, windowsKeyBuffer[126]);
-	kb.SimuLayerSetKeyState(CSK_RightCtrl, windowsKeyBuffer[124]);
+	// kb.SimuLayerSetKeyState(CSK_RightAlt, windowsKeyBuffer[126]);
+	// kb.SimuLayerSetKeyState(CSK_RightCtrl, windowsKeyBuffer[124]);
 
 	kb.SimuLayerSetKeyState(CSK_Up, windowsKeyBuffer[38]);
 	kb.SimuLayerSetKeyState(CSK_Down, windowsKeyBuffer[40]);
@@ -97,8 +97,8 @@ void WSL_KeyboardHelper::MoveWinBufIntoKeyBuf()
 	kb.SimuLayerSetKeyState(CSK_PageUp, windowsKeyBuffer[33]);
 	kb.SimuLayerSetKeyState(CSK_PageDown, windowsKeyBuffer[34]);
 
-	kb.SimuLayerSetKeyState(CSK_PrintScreen, windowsKeyBuffer[30]);
-	kb.SimuLayerSetKeyState(CSK_ScrollLock, windowsKeyBuffer[31]);
+	// kb.SimuLayerSetKeyState(CSK_PrintScreen, windowsKeyBuffer[30]);
+	// kb.SimuLayerSetKeyState(CSK_ScrollLock, windowsKeyBuffer[31]);
 	kb.SimuLayerSetKeyState(CSK_PauseAndBreak, windowsKeyBuffer[19]);
 
 	kb.SimuLayerSetKeyState(CSK_Keypad0, windowsKeyBuffer[96]);
@@ -112,8 +112,8 @@ void WSL_KeyboardHelper::MoveWinBufIntoKeyBuf()
 	kb.SimuLayerSetKeyState(CSK_Keypad8, windowsKeyBuffer[104]);
 	kb.SimuLayerSetKeyState(CSK_Keypad9, windowsKeyBuffer[105]);
 	kb.SimuLayerSetKeyState(CSK_KeypadDot, windowsKeyBuffer[110]);
-	kb.SimuLayerSetKeyState(CSK_KeypadNumLock, windowsKeyBuffer[122]);
-	kb.SimuLayerSetKeyState(CSK_KeypadEnter, windowsKeyBuffer[121]);
+	// kb.SimuLayerSetKeyState(CSK_KeypadNumLock, windowsKeyBuffer[122]);
+	// kb.SimuLayerSetKeyState(CSK_KeypadEnter, windowsKeyBuffer[121]);
 	kb.SimuLayerSetKeyState(CSK_KeypadAdd, windowsKeyBuffer[107]);
 	kb.SimuLayerSetKeyState(CSK_KeypadMinus, windowsKeyBuffer[109]);
 	kb.SimuLayerSetKeyState(CSK_KeypadMultiply, windowsKeyBuffer[106]);
